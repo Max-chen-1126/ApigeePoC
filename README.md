199b229da7ea733fe62a530d79dad833d8b0e9fa
MaxChen <max.chen@hkmci.com>
2024-06-19 17:26:21 +0800

    Apigee 完整 Config 文件

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # ApigeePoC
 Help Eupfin Do Apigee PoC
 
-- TestAPI
+# apigee-org 中包含導出的所有的 apigee 资源
+- 通過工具 apigee-sackmesser：https://github.com/apigee/devrel/blob/main/tools/apigee-sackmesser/README.md
+- 通過開一台 VM :
+  - 安裝 git 後 Clone 上面的 apigee-sackmesser Repo
+  - 安裝 docker 
+  - 執行 command: ./build.sh -t apigee-sackmesser
+  - 實際導出 : docker run -v "$PWD":/opt/apigee apigee-sackmesser export --googleapi -o "euppoc-apigee" -t "$(gcloud auth print-access-token)"
+    - 要注意 docker 的執行者是否有權限問題： Docker 容器內部預設是以 root 身份執行，但當您將本機目錄掛載到容器內時，可能會因為本機目錄的權限設定導致容器內的 root 無法寫入
+    - 解決方法： 
+      1. 在 docker 的執行時，使用 -u 參數來指定容器內的使用者 : sudo docker run -u $(id -u) -v "$PWD":/opt/apigee apigee-sackmesser export --googleapi -o "euppoc-apigee" -t "$(gcloud auth print-access-token)"
+      2. 修改權限 sudo chmod g+w . 後再執行
+- 等待導出完成後即可將資料轉移到 GCS 中 : gsutil -m cp -r ~/devrel/tools/apigee-sackmesser/euppoc-apigee/* gs://my-apigee-backups/
+- 後續要在其他環境中 Deploy : docker run -v "$PWD":/opt/apigee apigee-sackmesser deploy -d /opt/apigee/euppoc-apigee -e "您的目標環境名稱" --googleapi -o "您的 Apigee X 組織名稱" -t "$(gcloud auth print-access-token)"
---
+++ apigee-org/proxies/alcohol_info_with_carstatus/apiproxy/resources/jsc/MergeResponse.js
@@ -0,0 +1,72 @@
+// 獲取 CalloutCarStatusResponse 和 CalloutAlcoholInfoResponse 變量
+var carStatusResponse = context.getVariable("CalloutCarStatusResponse.content");
+var alcoholInfoResponse = context.getVariable("CalloutAlcoholInfoResponse.content");
+
+// 檢查變量是否存在並且是有效的 JSON 字符串
+if (carStatusResponse && alcoholInfoResponse) {
+  try {
+    var carStatus = JSON.parse(carStatusResponse);
+    var alcoholInfo = JSON.parse(alcoholInfoResponse);
+
+    // 檢查 carStatus 和 alcoholInfo 是否包含 result 屬性
+    if (carStatus.result && alcoholInfo.result) {
+      var carResult = carStatus.result;
+      var alcoholResult = alcoholInfo.result;
+
+      // 檢查 carNumber 是否相同
+      if (carResult[0].carNumber === alcoholResult[0].carNumber) {
+        // 將 alcohol_info 加入 carResult 中
+        for (var i = 0; i < carResult.length; i++) {
+          carResult[i].alcohol_info = alcoholResult;
+        }
+
+        var response = {
+          responseMsg: carStatus.responseMsg,
+          result: carResult,
+          failResult: null,
+          responseStatus: carStatus.responseStatus,
+        };
+
+        // 設置 response
+        context.setVariable("response.content", JSON.stringify(response));
+      } else {
+        // carNumber 不相同，返回錯誤
+        context.setVariable(
+          "response.content",
+          JSON.stringify({
+            responseMsg: "ERROR",
+            result: null,
+            failResult: "carNumber mismatch between carStatus and alcoholInfo",
+            responseStatus: 400,
+          })
+        );
+      }
+    } else {
+      // 處理缺少 result 屬性的情況
+      throw new Error("Missing 'result' property in carStatus or alcoholInfo");
+    }
+  } catch (e) {
+    // 處理 JSON 解析錯誤或其他錯誤
+    context.setVariable(
+      "response.content",
+      JSON.stringify({
+        responseMsg: "ERROR",
+        result: null,
+        failResult: e.message,
+        responseStatus: 500,
+      })
+    );
+  }
+} else {
+  // 處理缺少變量的情況
+  context.setVariable(
+    "response.content",
+    JSON.stringify({
+      responseMsg: "ERROR",
+      result: null,
+      failResult:
+        "Missing CalloutCarStatusResponse or CalloutAlcoholInfoResponse",
+      responseStatus: 500,
+    })
+  );
+}
